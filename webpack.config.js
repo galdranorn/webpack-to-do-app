5fc3d862c65340aca36c214280d6ddb8bc3a7b16
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2018-12-31 11:32:30 +0100

    Change webpack configuration (per environment)

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,37 @@
 const path = require('path');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
+module.exports = (env) => {
+    const environment = env || 'production';
+
+    return {
+        mode: environment,
+        entry: './src/index.js',
         output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    { loader: 'style-loader'},
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true
-                        }
+            path: path.resolve(__dirname, 'build'),
+            filename: 'app.bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
                     }
-                ]
-            }
-        ]
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        { loader: 'style-loader'},
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
     }
 };
